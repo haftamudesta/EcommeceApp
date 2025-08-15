5c5c789c855fb32c820f0bbd2a7bbcaeb9018342
haftamudesta <desta451616@hotmail.com>
2025-08-15 11:32:51 -0800

    Add loading state

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import { routeConfig } from "../RoutersConfig";
+import { Suspense } from 'react';
 
 const AppRouter = () => {
   return (
     <Routes>{routeConfig.map(({path,element})=>(
-        <Route key={path} path={path} element={element}/>
+        <Route key={path} path={path} element={<Suspense key={path} fallback={<>Loading...</>}>{element}</Suspense>}/>
     ))}</Routes>
   )
 }
