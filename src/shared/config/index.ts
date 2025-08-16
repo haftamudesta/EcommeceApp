de05b39daa9261024ec3754d62100445d8fa8d14
haftamudesta <desta451616@hotmail.com>
2025-08-16 00:30:09 -0800

    Add theme test button

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,3 +1,4 @@
 import {routePaths,AppRoutes} from "./route/routepaths"
 import { Theme,ThemeContext,LOCAL_STORAGE_THEME_KEY,type ThemeType } from "./theme/ThemeContext";
-export {routePaths,AppRoutes,Theme,ThemeContext,LOCAL_STORAGE_THEME_KEY,type ThemeType}
+import { useTheme } from "./theme/UseTheme";
+export {routePaths,AppRoutes,Theme,ThemeContext,LOCAL_STORAGE_THEME_KEY,type ThemeType,useTheme}
