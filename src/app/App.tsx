68e6475ff0015ced52d4ff4f61e87917d185b476
haftamudesta <desta451616@hotmail.com>
2025-08-16 01:31:00 -0800

    Add internationalization

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,15 @@
 
 // import AppRouter from "@/providers/router/ui/AppRouter"
 // import AppRouter from "./providers/router/ui/AppRouter"
+import { Suspense } from "react"
 import {AppRouter} from "./providers"
 
 function App() {
   
   return (
-    <>
+    <Suspense fallback={<></>}>
       <AppRouter />
-    </>
+    </Suspense>
   )
 }
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "@/app/styles/index.scss"
 import App from '@/app/App.tsx'
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from '@/app/providers';
+import "@/shared/config/i18n/i18n"
 
 
 createRoot(document.getElementById('root')!).render(
