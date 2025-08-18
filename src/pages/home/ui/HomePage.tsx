eaf56fc968effac08bbbddeef93beea61d6acde9
haftamudesta <desta451616@hotmail.com>
2025-08-17 23:26:35 -0800

    Add reUsable input

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router"
 import styles from "./HomePage.module.scss"
 import { useTheme } from "@/shared/config"
 import { useTranslation } from "react-i18next";
-import {Button} from "@/shared/ui"
+import {Button,Input} from "@/shared/ui"
 
 const HomePage = () => {
   const {toggleTheme}=useTheme();
@@ -13,11 +13,11 @@ const HomePage = () => {
   return (
     <>
     <h1 className={styles.title}>{t("hello")}</h1>
+    <Input placeholder="Search..." />
     <Button  onClick={toggleTheme}>theme</Button>
     <Button theme='outline' onClick={changeLanguage}>{i18n.language}</Button>
     <Link to={'/login'}>Log In</Link>
     </>
   )
 }
-
 export default HomePage
