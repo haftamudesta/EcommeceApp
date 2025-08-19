9bd813d265cb6ccba96eadfd1e7a626beadc3ffd
haftamudesta <desta451616@hotmail.com>
2025-08-19 10:31:35 -0800

    Add footer and spinner

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,23 +1,19 @@
-import { Link } from "react-router"
-import styles from "./HomePage.module.scss"
-import { useTheme } from "@/shared/config"
-import { useTranslation } from "react-i18next";
-import {AppIcon, Button,Input} from "@/shared/ui";
-import Search from "@/shared/assets/icons/Search.svg?react"
-import { useState } from "react";
+import { Link } from "react-router";
+import styles from "./HomePage.module.scss";
+import {Spinner} from "@/shared/ui";
+
 import { Header } from "@/widgets/header";
+import { Footer } from "@/widgets/footer";
 const HomePage = () => {
-  const {t,i18n}=useTranslation()
-  const changeLanguage=()=>{
-    i18n.changeLanguage(i18n.language==="en"?"de":"en")
-  }
   return (
-    <>
-    <h1 className={styles.title}>{t("hello")}</h1>
-    <Header />
-    <Button theme='outline' onClick={changeLanguage}>{i18n.language}</Button>
-    <Link to={'/login'}>Log In</Link>
-    </>
+    <div className={styles.pageWrapper}>
+       <Header />
+      <main className={styles.content}>
+        <Spinner />
+        <Link to={'/login'}>Log In</Link>
+      </main>
+      <Footer />
+    </div>
   )
 }
 export default HomePage
