1fe5745131e905453e6ff895cf3d049b66dd8e09
haftamudesta <desta451616@hotmail.com>
2025-08-25 13:43:11 -0800

    Add tabs

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -5,6 +5,7 @@ import { Header } from "@/widgets/header";
 import { Footer } from "@/widgets/footer";
 import { useEffect, useState } from "react";
 import styles from "./HomePage.module.scss";
+import { Tabs } from "@/shared/ui";
 
 
 const HomePage = () => {
@@ -17,6 +18,14 @@ const HomePage = () => {
        <Header />
       <main className={styles.content}>
         <Spinner />
+        <Tabs defaultValue="1">
+          <Tabs.List>
+            <Tabs.Tirgger value="1">1</Tabs.Tirgger>
+            <Tabs.Tirgger value="2">2</Tabs.Tirgger>
+          </Tabs.List>
+          <Tabs.Content value="1">1</Tabs.Content>
+          <Tabs.Content value="2">2</Tabs.Content>
+        </Tabs>
         <Link to={'/login'}>Log In</Link>
         <Button onClick={()=>setError(true)}>Click</Button>
       </main>
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,5 +2,6 @@ import {Button} from "./button/Button";
 import { Input } from "./input/Input";
 import { AppIcon } from "./appIcons/AppIcon";
 import { Spinner } from "./spinner/Spinner";
+import { Tabs } from "./tabs/Tabs";
  
-export {Button,Input,AppIcon,Spinner}
+export {Button,Input,AppIcon,Spinner,Tabs}
