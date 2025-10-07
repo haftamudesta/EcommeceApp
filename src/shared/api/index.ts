89008c92dd3cdf2a1f3f9c6c362b50ff9a17ca24
haftamudesta <desta451616@hotmail.com>
2025-10-07 10:41:53 -0800

    Add RTK Query API configuration

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,4 +1,5 @@
 import { httpClient,setAuthFailureHandler } from "./axios/HttpClient";
 import { extractErrorMessage } from "./axios/extractErrorMessage";
+import { baseAPI } from "./rtk/baseAPI";
 
-export { httpClient,setAuthFailureHandler,extractErrorMessage }
+export { httpClient,setAuthFailureHandler,extractErrorMessage,baseAPI }
